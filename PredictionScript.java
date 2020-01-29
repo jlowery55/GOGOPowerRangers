094d683de3143a53811b0f8ce54aac5cdc6dee77
dylanzuber <60367680+dylanzuber@users.noreply.github.com>
2020-01-29 13:29:46 -0800

    Update PredictionScript.java

--- PredictionScript.java
+++ PredictionScript.java
@@ -26,13 +26,72 @@ public class PredictionScript {
     String fasta = "selected.fasta";
     Scanner fastaInput = new Scanner(new File(fasta));
     
-    Map<String, String> inputSequences = new HashMap<String, String>();
+    Set<String> sequenceSet = new HashSet<String>(); // set that will contain all sequence words
     while(fastaInput.hasNextLine()){
       String name = fastaInput.nextLine();
       String seq = fastaInput.nextLine();
       
-      inputSequences.put(name, seq);
+      List<String> words = new ArrayList<String>();
+      for(int i = 0; i < seq.length() - 4; i++){
+        String small = seq.substring(i, i+3);
+        String large = seq.substring(i, i+4);
+        
+        if(sequenceSet.contains(small)) {
+          words.add(small);
+        }
+        if(sequenceSet.contains(large)) {
+          words.add(large);
+        }
+      }
+      Map<String, Double> functionTotalProb = new HashMap<String, Double>();
+      for(int i = 0; i < words.size(); i++){
+        String word = words.get(i);
+        Map<String, Double> map = sequenceMap.get(seq);
+        for(String func : map.keySet()){
+          if(functionTotalProb.containsKey(func)){
+            double prob = functionTotalProb.get(func);
+            double newProb = map.get(func);
+            functionTotalProb.put(func, prop + newProb);
+          } else {
+            double newProb = map.get(func);
+            functionTotalProb.put(func, newProb);
+          }
+        }
+      }
+      String outFile = "help.txt";
+      PrintStream out = new PrintStream(new File(outFile));
+      System.setOut(out);
+      
+      for(String func : functionTotalProb.keySet)) {
+        double prob = funcitonTotalProb.get(func);
+        System.out.println(prob + " " + func);
+      }
+      
+      String inFile = "help.txt";
+      Scanner in = new Scanner(new File(inFile));
+      
+      Map<double, String> result = new TreeMap<double, String>();
+      while(in.hasNextLine()){
+        double proba = in.nextDouble();
+        String function = in.next();
+        result.add(proba, function);
+      }
+      
+      String res = "res.txt";
+      PrintStream fin = new PrintStream(new File(res));
+      System.setOut(fin);
+      int i = 0;
+      for(double value : result.keySet()){
+        i++;
+        String fun = result.get(value);
+        System.out.println(fun + " " + value);
+        if(i == 10) {
+          break;
+        }
+      }
+      
     }
+    
     
     
   }
