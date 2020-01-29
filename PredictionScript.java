d28fd4280300ea3ea774b9555b623cee4290d6d2
dylanzuber <60367680+dylanzuber@users.noreply.github.com>
2020-01-29 12:25:24 -0800

    Update PredictionScript.java

--- PredictionScript.java
+++ PredictionScript.java
@@ -1,1 +1,28 @@
+import java.util.*;
+import java.io.*;
 
+public class PredictionScript {
+  
+  public static void main(String[] args) throws new FileNotFoundException {
+    Map<String, Map<String, Double>> sequenceMap = new HashMap<String, Map<String, Double>>();
+    String fileName = "result.txt";
+    Scanner input = new Scanner(new File(fileName));
+    
+    while(input.hasNextLine()){
+      String line = input.nextLine();
+      
+      String seq = input.next();
+      String func = input.next();
+      double prob = input.nextDouble();
+      
+      Map<String, Double> funcToProb = new HashMap<String, Double>();
+      if(sequenceMap.containsKey(seq)) {
+        funcToProb = sequenceMap.get(seq);
+      }
+      funcToProb.add(func, prob);
+      sequenceMap.add(seq, funcToProb);
+    }
+    
+    
+  }
+}
